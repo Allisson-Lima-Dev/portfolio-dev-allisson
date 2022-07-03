b735643757e6f87e282eff969ae3409cd06e91cc
Allisson <allisson.lima.dev@gmail.com>
2022-07-03 20:48:13 -0300

    feat: adicioando mock para card

---
+++ src/mocks/dataCardAbout.ts
@@ -0,0 +1,20 @@
+export const DataAboutCard = [
+  {
+    icon: "bx:desktop",
+    title: "Desenvolvimento Web",
+    description:
+      "Desenvolvo tudo que realiza uma interação direta com o usuário, ou seja,toda a programação da interface gráfica de um sistema.",
+  },
+  {
+    icon: "ant-design:android-filled",
+    title: "Desenvolvimento Mobile",
+    description:
+      "Desenvolvo o mobile com framework React Native para o Android seguindo toda metódologia do front end.",
+  },
+  {
+    icon: "la:pencil-ruler",
+    title: "UI/UX Designer",
+    description:
+      "Estou estudando UI/UX para criação de interfaces para complementar o conhecimento da experiência do usuário.",
+  },
+];
