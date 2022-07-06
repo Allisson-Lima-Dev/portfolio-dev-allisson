37aee451bee0950c90aef2a79bf7441afd30841e
Allisson Lima <Allysoncosta@outlook.com>
2022-07-06 17:58:21 -0300

    feat: adicionando mock para skills e index component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import { Input } from "./input";
 import { Layout } from "./layout";
 import { Header } from "./header";
 import { CardAbout } from "./cards/cardAbout";
+import { CardSkill } from "./cards/cardSkills";
 import { RowSkills } from "./rowSkills";
 
-export { Input, Layout, Header, CardAbout, RowSkills };
+export { Input, Layout, Header, CardAbout, RowSkills, CardSkill };
---
+++ src/mocks/dataSkills.ts
@@ -0,0 +1,85 @@
+export const dataSkills = [
+  {
+    skill: "TypeScript",
+    level: "Intermediário",
+  },
+  {
+    skill: "Javascript",
+    level: "Intermediário",
+  },
+  {
+    skill: "React Native",
+    level: "Intermediário",
+  },
+  {
+    skill: "Next js",
+    level: "Avançado",
+  },
+  {
+    skill: "Node js",
+    level: "Básico",
+  },
+];
+
+export const iconsDataSkill = [
+  {
+    icon: "bxl:mongodb",
+    title: "MongoDB",
+  },
+  {
+    icon: "logos:git-icon",
+    title: "Git",
+  },
+  {
+    icon: "logos:figma",
+    title: "Figma",
+  },
+  {
+    icon: "vscode-icons:file-type-jest",
+    title: "Jest",
+  },
+  {
+    icon: "logos:material-ui",
+    title: "Material UI",
+  },
+  {
+    icon: "logos:bootstrap",
+    title: "BootStrap",
+  },
+  {
+    icon: "logos:redux",
+    title: "Redux",
+  },
+  {
+    icon: "simple-icons:chakraui",
+    title: "Chakra UI",
+  },
+  {
+    icon: "vscode-icons:file-type-html",
+    title: "Html",
+  },
+  {
+    icon: "vscode-icons:file-type-css",
+    title: "Css",
+  },
+  {
+    icon: "vscode-icons:file-type-styled",
+    title: "Styled Component",
+  },
+  {
+    icon: "logos:yarn",
+    title: "Yarn",
+  },
+  {
+    icon: "vscode-icons:file-type-vscode",
+    title: "VScode",
+  },
+  {
+    icon: "logos:react",
+    title: "React js",
+  },
+  {
+    icon: "logos:react-query-icon",
+    title: "React query",
+  },
+];
