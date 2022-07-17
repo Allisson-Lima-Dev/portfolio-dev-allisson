f83b26117233612d561a5181c9068d6b7840566a
Allisson Lima <Allysoncosta@outlook.com>
2022-07-17 17:34:04 -0300

    feat: adicionando mock para cards de skills

--- src/mocks/dataSkills.ts
+++ src/mocks/dataSkills.ts
@@ -83,3 +83,24 @@ export const iconsDataSkill = [
     title: "React query",
   },
 ];
+
+export const dataCardSoftSkill = [
+  {
+    icon: "clarity:design-line",
+    title: "Design incrivel",
+    description:
+      "Trabalho com a implementação fiel de interfaces presando pela melhor experiência do usuário para atender as expectativas de cada projeto.",
+  },
+  {
+    icon: "mdi:timer-sand",
+    title: "Sou ágil",
+    description:
+      "Utilizo metodologias e planejamentos otimizados que dinamizam o fluxo de produção. Através de feedback e interações constantes, garantido a agilidade e eficiência do processo.",
+  },
+  {
+    icon: "fa-solid:laptop-code",
+    title: "Código Limpo",
+    description:
+      "Tenho como minha principal diretriz durante o desenvolvimento a qualidade do código seguindo as melhores práticas do mercado.",
+  },
+];
