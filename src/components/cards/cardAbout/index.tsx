b4fd9f6546651cfa25e58c6dd066e903d6d468aa
Allisson Lima <Allysoncosta@outlook.com>
2022-07-17 17:33:38 -0300

    refactor: adicionando type para card no component

--- src/components/cards/cardAbout/index.tsx
+++ src/components/cards/cardAbout/index.tsx
@@ -6,15 +6,21 @@ interface IPropsCard<T = string> {
   icon: T;
   title: T;
   description: T;
+  type?: "about" | "skill";
 }
 
-export function CardAbout({ description, icon, title }: IPropsCard) {
+export function CardAbout({
+  description,
+  icon,
+  title,
+  type = "about",
+}: IPropsCard) {
   return (
     <Flex
       w="307px"
       h="350px"
       bg="#131313"
-      align={"center"}
+      align={type === "about" ? "center" : "flex-start"}
       flexDir="column"
       borderRadius={"10px"}
       boxShadow="xl"
@@ -23,16 +29,26 @@ export function CardAbout({ description, icon, title }: IPropsCard) {
     >
       <Icon
         icon={icon}
-        width={"75px"}
+        width={type === "about" ? "75px" : "50px"}
         height={"71px"}
         color="#6EDB5C"
         style={{ marginTop: 50 }}
         className="icon"
       />
-      <Text fontSize={"18px"} color="#fff" mt="16px" className="title">
+      <Text
+        fontSize={type === "about" ? "18px" : "20px"}
+        color="#fff"
+        mt="16px"
+        className="title"
+      >
         {title}
       </Text>
-      <Text fontSize={"14px"} color="#fff" mt="16px" textAlign={"center"}>
+      <Text
+        fontSize={"14px"}
+        color="#fff"
+        mt={type === "about" ? "16px" : "20px"}
+        textAlign={type === "about" ? "center" : "left"}
+      >
         {description}
       </Text>
     </Flex>
